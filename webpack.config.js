a97fbab94844ca11fff5f84b4aeef39d142ce55d
“Saurja” <saurjaghoshofficial@gmail.com>
2020-07-08 22:41:37 +0530

    Responsive image

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const { postcssPlugin } = require('cssnano')
 
 const postcssPlugins = [
     require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
@@ -13,6 +14,15 @@ module.exports = {
     output: {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
+    },
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
     },
     mode: 'development',
     watch: true,
