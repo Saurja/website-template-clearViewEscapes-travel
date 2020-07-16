001358dc8f1c9527f21573f5fc344b8c269e419c
“Saurja” <saurjaghoshofficial@gmail.com>
2020-07-16 11:08:47 +0530

    added hamburger icon

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,10 @@
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+
 
 if (module.hot) {
     module.hot.accept()
 }
+
+/*      Code Below      */
+let mobileMenu = new MobileMenu();
