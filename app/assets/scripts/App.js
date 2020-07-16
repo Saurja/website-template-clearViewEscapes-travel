7aebc6a82aa781a1d518fb6d52b4cdd0d739e337
“Saurja” <saurjaghoshofficial@gmail.com>
2020-07-16 15:13:09 +0530

    header v1

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,7 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader';
 
 
 if (module.hot) {
@@ -11,6 +12,7 @@ if (module.hot) {
 
 
 let mobileMenu = new MobileMenu();
+let stickyHeader = new StickyHeader();
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
